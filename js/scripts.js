1e1bf73463ba8f4e6455f9a784b7b74971ffdb61
Guest User <Guest@epicodus-20.local>
2017-08-17 16:17:37 -0700

    add first draft

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
-function clicked(event){
-  event.preventDefault();
-  var stringIn = $("input#stringIn").val();
+var convertToRoman = function makeRoman(num) {
+    var roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    var arabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 
-  $("#stringOut").html(stringIn)
+    var romanized = [''];
+
+    for (var i=0; i < arabic.length; i++){
+      while (arabic[i] <= num) {
+        romanized += roman[i];
+        num -= arabic[i];
+      }
+    }
+    return romanized;
+  };
+
+$(document).ready(function(){
+ $('form#roman-num').submit(function(event) {
+    event.preventDefault();
+    var someInput = $('input#stringIn').val('arabic');
+    var someOutput = $('')
+    alert(convertToRoman);
+
+  })
 });
